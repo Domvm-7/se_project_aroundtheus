e7f1154153e5311e70ce00efe034ab6a3f553cea
DominicMartinez <domvm.7@gmail.com>
2023-10-02 16:30:59 -0700

    New update for my project

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,6 +6,14 @@ export default class PopupWithForm extends Popup {
     this._popupForm = this.popupElement.querySelector(".popup__form");
     this._handleFormSubmit = handleFormSubmit;
   }
+  _getInputValues() {
+    const inputList = Array.from(this._form.querySelectorAll(".popup__input"));
+    const inputValues = {};
+    inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
 
   close() {
     this._popupForm.reset();
