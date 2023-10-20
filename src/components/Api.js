74f77623df05082299572905f5de7dafd82e70db
DominicMartinez <domvm.7@gmail.com>
2023-10-20 16:49:39 -0700

    New update for my project

--- src/components/Api.js
+++ src/components/Api.js
@@ -8,13 +8,16 @@ class Api {
       headers: {
         authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
       },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   // other methods for working with the API
