c54a37fce1d888d8f3c427743aa86d972c2e53da
DominicMartinez <domvm.7@gmail.com>
2024-01-09 18:12:10 -0800

    Avatar image changes when saved

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,8 +129,7 @@ function handleAvatarSubmit({ avatar }) {
     .updateAvatar({ avatar: avatar })
     .then((updatedUserInfo) => {
       console.log("Avatar updated successfully:", updatedUserInfo);
-      document.querySelector(".profile__avatar-edit").src =
-        updatedUserInfo.avatar;
+      document.querySelector(".profile__image").src = updatedUserInfo.avatar;
     })
     .catch((error) => {
       console.error("Error updating avatar:", error);
